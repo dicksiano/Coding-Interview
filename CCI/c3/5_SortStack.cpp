2ac34d8950a640de33675c42562f273fc019445b
dicksiano <dicksianomelo@gmail.com>
2018-10-03 22:53:03 -0300

    Better code

--- CCI/c3/5_SortStack.cpp
+++ CCI/c3/5_SortStack.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 /*
-	Pop all elements that are bigger than the new one.
-	Put all elements bigger in the aux stack.
-	In the end, put all elements in the aux stack in the stack.
+	Pop all elements that are bigger than the current one.
+	In the end, put all elements from the aux stack in the principal stack.
 */
 void sortStack( stack<int> &s ) {
 	stack<int> aux;
@@ -14,18 +13,11 @@ void sortStack( stack<int> &s ) {
 	while(!s.empty()) {
 		curr = s.top(); s.pop();
 
-		if(aux.empty()) 
-			aux.push(curr);
-		else {
-			while(curr < aux.top() ) {
-				s.push(aux.top());
-				aux.pop();
-
-				if(aux.empty()) // Avoid crash of using top() in a empty queue
-					break;
-			}
-			aux.push(curr);
+		while(!aux.empty() && curr < aux.top() ) {
+			s.push(aux.top());
+			aux.pop();
 		}
+		aux.push(curr);
 	}
 
 	while(!aux.empty()) {
