10c581bc172396a15f1817998a40401be4cece9a
dicksiano <dicksianomelo@gmail.com>
2018-10-04 21:52:35 -0300

    Much better code

--- CCI/c5/3_FlipBitToWin.cpp
+++ CCI/c5/3_FlipBitToWin.cpp
@@ -2,34 +2,17 @@
 using namespace std;
 
 int flipBitToWin(int x) {
-	int current = 0;
-	int maxi = -1;
-	int newStart;
-	int hasZero = 0;
+	int current = 0, prev = 0, maxi = -1;
 
 	for (int i = 0; (1 << i) <= x; ++i) {
-		if(x & (1 << i)) {
-			current++;
-			maxi = max(current,maxi);
-
-			if(x & (1 << newStart) == 0)
-				newStart = i-1;
-		}
+		if(x & (1 << i)) current++;
 		else {
-			if(!hasZero) {
-				hasZero = 1;
-				current++;
-				maxi = max(current,maxi);
-				newStart = i;
-			}
-			else {
-				hasZero = 0;
-				current = 0;
-				i = newStart;
-			}
+			prev = current;
+			current = 0;
 		}
+		maxi = max(current + prev, maxi);		
 	}
-	return maxi;
+	return maxi + 1;
 }
 
 int main() {
