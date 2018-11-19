22058bf15c2ec62ec49bddbc02abd44569419c0f
dicksiano <dicksianomelo@gmail.com>
2018-11-19 12:58:05 -0200

    Clean code

--- CCI/c8/PermutationWithoutDups.cpp
+++ CCI/c8/PermutationWithoutDups.cpp
@@ -2,33 +2,42 @@
 using namespace std;
 
 /*
-  Time:  O(n!)
+  (xyz, 1) => xz
+*/
+string removeChar(string s, int i) {
+    string aux = "";
+    for (int j = 0; j < s.size(); ++j) 
+      if( i != j ) aux = aux + s[j];
+    return aux;
+}
+
+/*
+  xy => {xy, yx}
+*/
+vector<string> perm2(string s) {
+    vector<string> res;
+    res.push_back(s);
+    reverse(s.begin(), s.end());
+    res.push_back(s);
+    return res;
+}
+
+/*
+  Time:  O(n n!)
   Space: O(n!)
 */
 vector<string> permutation(string s) {
-  vector<string> res;
-  if(s.size() == 2) {
-    res.push_back(s);
-    string aux;
-    aux.push_back(s[1]);
-    aux.push_back(s[0]);
-    res.push_back(aux);
-    return res;
-  }
-  vector<string> vAux;
+  if(s.size() == 2) return perm2(s);  // Base case
+  vector<string> res, vAux;
 
   for (int i = 0; i < s.size(); ++i) {
-    string aux = "";
-        
-    for (int j = 0; j < s.size(); ++j) 
-      if( i != j ) aux = aux + s[j];
-    vAux.clear();
-    vAux = permutation(aux);
+    vAux = permutation( removeChar(s, i) );
     for(vector<string>::iterator it = vAux.begin(); it != vAux.end(); it++)
       res.push_back(s[i] + *it);
   }
   return res;
 }
+
 int main() {
   string str = "abcd";
   vector<string> res = permutation(str);
