5b52bd0d0bc4769e718d47e3d2eb03241a6fa28f
dicksiano <dicksianomelo@gmail.com>
2018-11-19 17:20:48 -0200

    Problem 10

--- CCI/c8/Coins.cpp
+++ CCI/c8/Coins.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 #define MAX_SIZE 1001
-#define NUM_OF_STEPS 3
 
 /*
   Classic Coin DP!
@@ -10,24 +9,25 @@ using namespace std;
   Time: O(n * m)
   Space: O(n * m)
 */
-int backtrack(int x, int state, vector<int> coins, int dpTable[][NUM_OF_STEPS]) {
-  if(x < 0 || state >= coins.size()) return 0;  // Invalid choice
-  if(x == 0) return 1;                          // Finished the staircase
+int backtrack(int x, int state, vector<int> coins, int dpTable[][4]) {
+  if(x < 0 || state >= coins.size()) return 0;    // Invalid choice
+  if(x == 0) return 1;                            // Finished the staircase
 
-  if(dpTable[x][state] == -1) 
-    dpTable[x][state] = backtrack(x, state+1, coins, dpTable) + backtrack(x-coins[state], state, coins, dpTable);
+  if(dpTable[x][state] == -1) {
+    dpTable[x][state]   = backtrack(x,state+1,coins,dpTable) + backtrack(x-coins[state],state,coins,dpTable);
+  }
   
   return dpTable[x][state];
 }
 
 int numOfWays(int x) {
-  int dpTable[MAX_SIZE][NUM_OF_STEPS];
+  int dpTable[MAX_SIZE][4];
   vector<int> coins; 
   
-  coins.push_back(1); coins.push_back(2);coins.push_back(3); // 3 possible movements
+  coins.push_back(1); coins.push_back(5); coins.push_back(10); coins.push_back(25); // Possible coins
 
   memset(dpTable, -1, sizeof dpTable);
-  return backtrack(x,0,coins,dpTable);
+  return backtrack(x, 0, coins, dpTable);
 }
 
 int main() {
