7e1f81b0499809045106ae5c47ab4c93a852dd0c
Dicksiano <dicksianomelo@gmail.com>
2019-02-01 23:19:57 -0800

    3 finished

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxi = 0, j = 1;
-        int mask[260]; memset(mask, 0, sizeof(mask));
+        int maxi = 0, j = 0;
+        vector<int> mask; mask.assign(260, 0);
         
         for(int i = 0; i < s.size(); i++) {
-            if(!mask[ s[i] ]) {
-                mask[ s[i] ] = 1; 
+            mask[ s[i] ] = 1; 
                 
-                for(j = max(j, i+1); (j < s.size()) && !mask[ s[j] ]; j++) 
-                    mask[ s[j] ] = 1;
+            for(j = j+1; (j < s.size()) && !mask[ s[j] ]; j++) 
+                mask[ s[j] ] = 1;
                 
-                maxi = max(maxi, j-i);   
-            }
-            cout << i << " - " << s[i] << ", " << j << ": " << maxi << endl; 
-            if(s[i] != s[j] || i == j-1) mask[ s[i] ] = 0;     
+            maxi = max(maxi, j-i);              
+            for(;  s[i] != s[j]; i++) mask[ s[i] ] = 0;     
         }
         return maxi;
     }
