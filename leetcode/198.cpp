a97d18992c8ce2ee6b91ad1cfcbb07e166f27b77
Dicksiano <dicksianomelo@gmail.com>
2019-04-16 21:00:42 -0300

    Better solution, from O(n) space to O(1) space

--- leetcode/198.cpp
+++ leetcode/198.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size() < 1) return 0;
-        if(nums.size() < 2) return nums[0];
         
-        int N = nums.size(), maxi = 0;
-        int dp[N] = {0};
-        
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        maxi = max(dp[0], dp[1]);
-        
-        for(int i = 2; i < N; i++) {
-            dp[i] = nums[i] + (i > 2 ? max(dp[i-2], dp[i-3]) : dp[i-2]);
-            maxi = max(maxi, dp[i]);
+        int prev = 0, curr = 0, aux;        
+        for(int i = 0; i < nums.size(); i++) {
+            aux = curr;
+            curr = max(nums[i] + prev, curr);
+            prev = aux;
         }
-        return maxi;
+        return max(prev, curr);
     }
 };
