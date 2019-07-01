4b6aff8f92e97dbae5daeb0bbd8ae48e5e7d8043
Dicksiano <dicksianomelo@gmail.com>
2019-07-01 16:55:54 -0300

    72

---
+++ leetcode/72.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector< vector<int> > dp( word1.size() + 1, vector<int>(word2.size() + 1, 0) );
+        for(int i = 0; i <= word1.size(); i++) dp[i][0] = i;
+        for(int i = 0; i <= word2.size(); i++) dp[0][i] = i;
+        
+        for(int i = 1; i <= word1.size(); i++)
+            for(int j = 1; j <= word2.size(); j++)
+                dp[i][j] = (word1[i-1] == word2[j-1]) ? dp[i-1][j-1] : (1 + min(dp[i-1][j-1], min( dp[i-1][j], dp[i][j-1] ) ) );
+        return dp[word1.size()][word2.size()];
+    }
+};
